b9908ae7da55f4f6ceda618bb8250da841c58783
bimonestle <bimonestle@yahoo.co.id>
2020-02-29 20:38:08 +0700

    Refine the Add & Do command

--- cmd/do.go
+++ cmd/do.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -11,20 +12,19 @@ var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Marks a task as complete",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("do called")
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument: ", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		fmt.Println(ids)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(doCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// doCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// doCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
