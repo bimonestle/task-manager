fce158f8d5143f7f819a5abf7238b99651f6d655
bimonestle <bimonestle@yahoo.co.id>
2020-03-02 22:28:12 +0700

    Add AllCompleted() to 'task list' comand pt.2

--- cmd/complete.go
+++ cmd/complete.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/bimonestle/go-exercise-projects/07.CLI-Task-Manager/task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -12,6 +15,18 @@ var completeCmd = &cobra.Command{
 	Short: "Lists all the completed tasks",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("complete called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Someting went wrong: ", err)
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You don't have any completed tasks. Be productive!")
+			return
+		}
+		for i, task := range tasks {
+			fmt.Printf("You have completed: %d. %s, Key=%d\n", i+1, task.Value, task.Key)
+		}
 	},
 }
 
